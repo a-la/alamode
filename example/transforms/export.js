fe484f8a797bd8a18b354a92a762096b703dea33
Anton <anton@adc.sh>
2018-08-13 17:16:38 +0100

    export example

--- example/transforms/export.js
+++ example/transforms/export.js
@@ -1,42 +1,11 @@
-/**
- * Example 1: named export (const).
- */
-export const example1 = async () => {
-  console.log('named export 1')
-}
+export async function example () {}
 
-/**
- * Example 2: named export (function).
- */
-export function example2() {
-  console.log('named export 2')
-}
+const example2 = () => {}
 
-/**
- * Example 3: declare a function and export later.
- */
-function example3() {
-  console.log('named export 3')
-}
-
-/**
- * Example 4: declare an async function and export later with an alias.
- */
-const example4 = async () => {
-  console.log('named export 4')
-}
-
-export { example3, example4 as alias4 }
-
-/**
- * Default Class Example.
- */
 export default class Example {
-  /**
-   * A constructor for the example.
-   * @constructor
-   */
   constructor() {
-    console.log('default export')
+    example()
   }
 }
+
+export { example2 as alias }
