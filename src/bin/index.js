5ebe4e04ca81b5629eaa46f148852d0d6402ad9e
Anton <anton@adc.sh>
2018-08-22 02:14:57 +0100

    transpile ref

--- src/bin/index.js
+++ src/bin/index.js
@@ -1,30 +1,24 @@
 #!/usr/bin/env node
 import argufy from 'argufy'
-import readDirStructure from '@wrote/read-dir-structure'
-import ensurePath from '@wrote/ensure-path'
 import usually from 'usually'
-import { resolve, join } from 'path'
-import { lstatSync } from 'fs'
-import { debuglog } from 'util'
-
 import { version } from '../../package.json'
 import catcher from './catcher'
-import { copyMode } from '../lib'
-import writeSourceMap from '../lib/source-map'
-import { transformStream } from '../lib/transform'
-
-const LOG = debuglog('alamode')
+import { transpile } from './transpile'
 
 const {
   input: _input,
   output: _output,
   version: _version,
   help: _help,
+  ignore: _ignore,
+  noSourceMaps: _noSourceMaps,
 } = argufy({
   input: { command: true },
   output: { short: 'o' },
   version: { short: 'v', boolean: true },
   help: { short: 'h', boolean: true },
+  ignore: { short: 'i' },
+  noSourceMaps: { short: 's', boolean: true },
 })
 
 if (_help) {
@@ -34,6 +28,11 @@ if (_help) {
 either a directory or a file.`,
       '--output, -o': `Location to save results to. Passing "-"
 will print to stdout when source is a file.`,
+      '--help, -h': 'Display help information.',
+      '--version, -v': 'Show version.',
+      '--ignore, -i': `Paths to files to ignore, relative to
+source.`,
+      '--noSourceMaps, -s': 'Don\'t generate source maps.',
     },
     description: 'A tool to transpile JavaScript packages using regular expressions.',
     line: 'alamode source [-o destination]',
@@ -46,67 +45,15 @@ will print to stdout when source is a file.`,
   process.exit()
 }
 
-const processFile = async (input, relPath, name, output) => {
-  const isOutputStdout = output == '-'
-  const file = join(relPath, name)
-  const source = join(input, file)
-
-  const outputDir = isOutputStdout ? null : join(output, relPath)
-  const destination = isOutputStdout ? '-' : join(outputDir, name)
-  LOG(file)
-
-  await ensurePath(destination)
-
-  const originalSource = await transformStream({
-    source,
-    destination,
-  })
-
-  if (output != '-') {
-    copyMode(source, destination)
-    writeSourceMap({
-      destination,
-      file,
-      name,
-      outputDir,
-      source,
-      originalSource,
-    })
-  }
-}
-
-const processDir = async (input, output, relPath = '.') => {
-  const path = resolve(input, relPath)
-  const { content } = await readDirStructure(path)
-  const k = Object.keys(content)
-  await k.reduce(async (acc, name) => {
-    await acc
-    const { type } = content[name]
-    if (type == 'File') {
-      await processFile(input, relPath, name, output)
-    } else if (type == 'Directory') {
-      const newRelPath = join(relPath, name)
-      await processDir(input, output, newRelPath)
-    }
-  }, Promise.resolve())
-}
-
-const run = async (input, output = '-') => {
-  if (!input) throw new Error('Please specify the source file or directory.')
-
-  const ls = lstatSync(input)
-  if (ls.isDirectory()) {
-    if (!_output) throw new Error('Please specify the output directory.')
-    await processDir(input, _output)
-  } else if (ls.isFile()) {
-    await processFile(input, '.', '.', output)
-  }
-  if (output != '-') process.stdout.write(`Transpiled code saved to ${output}\n`)
-}
-
 (async () => {
   try {
-    await run(_input, _output)
+    const ignore = _ignore ? _ignore.split(','): []
+    await transpile({
+      input: _input,
+      output: _output,
+      noSourceMaps: _noSourceMaps,
+      ignore,
+    })
   } catch (err) {
     catcher(err)
   }
---
+++ src/bin/transpile.js
@@ -0,0 +1,104 @@
+import { join, basename, dirname } from 'path'
+import { lstatSync } from 'fs'
+import readDirStructure from '@wrote/read-dir-structure'
+import ensurePath from '@wrote/ensure-path'
+import { debuglog } from 'util'
+import { copyMode } from '../lib'
+import writeSourceMap from '../lib/source-map'
+import { transformStream } from '../lib/transform'
+
+const LOG = debuglog('alamode')
+
+const processFile = async ({
+  input, relPath, name, output, ignore, noSourceMaps,
+}) => {
+  const file = join(relPath, name)
+  if (ignore.includes(file)) return
+
+  const isOutputStdout = output == '-'
+  const source = join(input, file)
+
+  const outputDir = isOutputStdout ? null : join(output, relPath)
+  const destination = isOutputStdout ? '-' : join(outputDir, name)
+  LOG(file)
+
+  await ensurePath(destination)
+
+  const originalSource = await transformStream({
+    source,
+    destination,
+  })
+
+  if (output != '-') {
+    copyMode(source, destination)
+    if (noSourceMaps) return
+    writeSourceMap({
+      destination,
+      file,
+      name,
+      outputDir,
+      source,
+      originalSource,
+    })
+  }
+}
+
+const processDir = async ({
+  input,
+  output,
+  relPath = '.',
+  ignore = [],
+  noSourceMaps,
+}) => {
+  const path = join(input, relPath)
+  const { content } = await readDirStructure(path)
+  const k = Object.keys(content)
+  await k.reduce(async (acc, name) => {
+    await acc
+    const { type } = content[name]
+    if (type == 'File') {
+      await processFile({
+        input, relPath, name, output, ignore, noSourceMaps,
+      })
+    } else if (type == 'Directory') {
+      const newRelPath = join(relPath, name)
+      await processDir({
+        input,
+        output,
+        ignore,
+        relPath: newRelPath,
+        noSourceMaps,
+      })
+    }
+  }, Promise.resolve())
+}
+
+export const transpile = async ({
+  input,
+  output = '-',
+  ignore = [],
+  noSourceMaps,
+}) => {
+  if (!input) throw new Error('Please specify the source file or directory.')
+
+  const ls = lstatSync(input)
+  if (ls.isDirectory()) {
+    if (!output) throw new Error('Please specify the output directory.')
+    await processDir({
+      input,
+      output,
+      ignore,
+      noSourceMaps,
+    })
+  } else if (ls.isFile()) {
+    await processFile({
+      input: dirname(input),
+      relPath: '.',
+      name: basename(input),
+      output,
+      ignore,
+      noSourceMaps,
+    })
+  }
+  if (output != '-') process.stdout.write(`Transpiled code saved to ${output}\n`)
+}
