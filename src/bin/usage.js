1bee72cc92961d8828c0d2769aa4ff6d8eb3eca7
Anton <anton@adc.sh>
2018-09-15 05:06:29 +0300

    usage

---
+++ src/bin/usage.js
@@ -0,0 +1,23 @@
+import usually from 'usually'
+
+const getUsage = () => {
+  const usage = usually({
+    usage: {
+      source: `Location of the input to the transpiler,
+either a directory or a file.`,
+      '--output, -o': `Location to save results to. Passing "-"
+will print to stdout when source is a file.`,
+      '--help, -h': 'Display help information.',
+      '--version, -v': 'Show version.',
+      '--ignore, -i': `Paths to files to ignore, relative to
+source.`,
+      '--noSourceMaps, -s': 'Don\'t generate source maps.',
+    },
+    description: 'A tool to transpile JavaScript packages using regular expressions.',
+    line: 'alamode source [-o destination]',
+    example: 'alamode src -o build',
+  })
+  return usage
+}
+
+export default getUsage
