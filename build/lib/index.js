e1c7d69035567a7d889d746fb8856a186baf135d
Anton <anton@adc.sh>
2019-04-24 11:07:05 +0300

    rm build

--- build/lib/index.js
+++
@@ -1,9 +0,0 @@
-const { chmodSync, lstatSync } = require('fs');
-
-       const copyMode = (input, output) => {
-  const ls = lstatSync(input)
-  const { mode } = ls
-  chmodSync(output, mode)
-}
-
-module.exports.copyMode = copyMode
